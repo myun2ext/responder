00454804f73f918f314828d343feaee12c61b2df
myun2 <myun2@nwhite.info>
2015-04-08 19:52:09 +0900

    linux socket

---
+++ linux/socket.hpp
@@ -0,0 +1,22 @@
+#ifndef __MYUN2__RESPONDER__LINUX__SOCKET__HPP__
+#define __MYUN2__RESPONDER__LINUX__SOCKET__HPP__
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+namespace myun2
+{
+	namespace responder
+	{
+		struct linux_socket_desc
+		{
+			typedef int sock_type;
+
+			static int close(sock_type s) { return close(s); }
+			static bool valid_socket(sock_type s) { return s != -1; }
+		};
+	}
+}
+
+#endif//__MYUN2__RESPONDER__LINUX__SOCKET__HPP__
