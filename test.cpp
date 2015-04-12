f213b6e37a103a3caa43e1ff6a0d054ae55946f4
myun2 <myun2@nwhite.info>
2015-04-13 04:56:41 +0900

    windows fdopen

--- test.cpp
+++ test.cpp
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "myun2/responder/listener.hpp"
 
-#ifndef WIN32
+#ifdef WIN32
+	#include <winsock2.h>
+	#include <io.h>
+	#include <fcntl.h>
+	#define SO_SYNCHRONOUS_NONALERT 0x20
+	#define SO_OPENTYPE     0x7008
+#else
 	#include <errno.h>
 #endif
 
@@ -12,7 +18,11 @@ int accepted(struct sockaddr_in addr, socket_type sock)
 {
 	printf("accepted!!\n");
 	FILE *fp;
+#ifdef WIN32
+	fp = fdopen(_open_osfhandle(sock, _O_RDONLY), "rb");
+#else
 	fp = fdopen(sock, "r");
+#endif
 
 	char buffer[1024];
 	while(1)
@@ -30,6 +40,8 @@ int main()
 	try {
 #ifdef WIN32
 		wsa_cleaner _wsa_cleaner;
+		int sockopt = SO_SYNCHRONOUS_NONALERT;
+		setsockopt(INVALID_SOCKET, SOL_SOCKET, SO_OPENTYPE, (char *)&sockopt, sizeof(sockopt));
 #endif
 		listener server(8000, 10, accepted);
 	}
